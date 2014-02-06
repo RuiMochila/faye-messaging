80b570d006bf2d5c8d47bb7702b4d76b3573e638
RuiMochila <rui.mochila@gmail.com>
2014-02-06 17:45:02 +0000

    Just a space

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
 
+  # curl http://localhost:9292/faye -d 'message={[channel]("/messages/new",) [data]("hello")}'
   def broadcast(channel, &block)
     message = {:channel => channel, :data => capture(&block), :ext => {:auth_token => FAYE_TOKEN}}
     uri = URI.parse("http://localhost:9292/faye")
